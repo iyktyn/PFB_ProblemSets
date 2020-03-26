a194fd177a91c4fc123d26ee9c6244bb0af96937
iyktyn <48490460+iyktyn@users.noreply.github.com>
2020-03-26 16:19:00 -0500

    updates

--- namescript.py
+++ namescript.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
-print('My name: Kyle')
+
+import sys
+
+name = sys.argv[1] #get name from cli
+color = sys.argv[2] #get color from cli
+activity = sys.argv[3] #get name from cli
+animal = sys.argv[4] #get animal from cli
+
+print('My name: ' , name , 'and my favorite color is ' , color , 'and my favorite animal is ' , animal , 'and my favorite acitivty is ' , activity)  
