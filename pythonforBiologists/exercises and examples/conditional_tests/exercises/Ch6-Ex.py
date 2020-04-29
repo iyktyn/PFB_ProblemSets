f967bd54d5f19082aa6db0ff96069324b712284f
Kyle Weaver <kjw@Kyles-Mac-mini.local>
2020-04-29 16:30:26 -0500

    Modified Ch6
    Added more exercises to Ch6-Ex.py

--- pythonforBiologists/exercises and examples/conditional_tests/exercises/Ch6-Ex.py
+++ pythonforBiologists/exercises and examples/conditional_tests/exercises/Ch6-Ex.py
@@ -16,8 +16,25 @@ def Len_range(csv, low = 0, high = 500):
 	for line in csv:
 		if len(line[1]) > low and len(line[1]) < high:
 			print(line[2])
+
+def AT_content(seq):
+	seq = seq.upper()
+	count = 0
+	for base in seq:
+		if base == 'A' or base == 'T':
+			count =+ 1
+	AT_cont = count / len(seq)
+	return AT_cont
+
+def AT_content_and_expression(csv,exp):
+	for line in csv:
+		if AT_content(line[1]) < 0.5 and int(line[3]) > exp:
+			print(line[2])
+
 print("Starting")
 Sev_species(data, ['Drosophila melanogaster', 'Drosophila simulans'])
 print("Finished with first Ex")
 Len_range(data, 90,110)
 print("Finished with second Ex")
+AT_content_and_expression(data,200)
+print("Finished with third Ex")
