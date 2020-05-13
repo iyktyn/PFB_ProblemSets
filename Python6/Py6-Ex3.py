3309731526f8b88801fabf391c32146580ca6aa5
Kyle Weaver <kjw@Kyles-Mac-mini.local>
2020-05-13 14:32:34 -0500

    Added Ex3
    Added the files from exercise 3.

---
+++ Python6/Py6-Ex3.py
@@ -0,0 +1,17 @@
+#! /usr/local/bin/python3
+
+def rc(dna):
+	dna = dna.upper()
+	dna = dna.replace('A','t')
+	dna = dna.replace('T','a')
+	dna = dna.replace('C','g')
+	dna = dna.replace('G','c')
+	dna = dna[::-1]
+	return dna.upper()
+
+with open("Python_06.seq.txt",'r') as seq:
+	for line in seq:
+		line = line.rstrip()
+		gene,dna = line.split()
+		dnarc = rc(dna)
+		print(">" + gene + " Reverse complement\n" + dnarc + "\n")
