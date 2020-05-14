5b08a3e5607c7c6d95954df2d2c3d95d79d4aca9
Kyle Weaver <iyktyn@gmail.com>
2020-05-14 14:36:49 -0500

    Added Ex5
    Added files from Ex5

--- Python6/Py6-Ex2.py
+++ Python6/Py6-Ex2.py
@@ -1,7 +1,7 @@
 #! /usr/local/bin/python3
 #Adding for testing
 
-with open("Python_06.txt",'r') as text, open("Python_06_uc.txt",'w') as output:
+with open("Python_06.seq.txt",'r') as text, open("Python_06_uc.txt",'w') as output:
 	for line in text:
 		line = line.rstrip().upper()
 		output.write (line + "\n")
